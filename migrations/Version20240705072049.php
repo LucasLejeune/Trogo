33db8956fc780c6f0007a125cd88f84eaa32525c
LucasLejeune <lucaslejeune0@gmail.com>
2024-07-05 09:48:38 +0200

    Add placeholder image

--- migrations/Version20240705072049.php
+++ migrations/Version20240705072049.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240703091830 extends AbstractMigration
+final class Version20240705072049 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,6 @@ final class Version20240703091830 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE workout_user DROP FOREIGN KEY FK_F51DD535A6CCCFC9');
-        $this->addSql('ALTER TABLE workout_user DROP FOREIGN KEY FK_F51DD535A76ED395');
         $this->addSql('DROP TABLE workout_user');
         $this->addSql('ALTER TABLE workout ADD user_id INT NOT NULL');
         $this->addSql('ALTER TABLE workout ADD CONSTRAINT FK_649FFB72A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
@@ -32,8 +30,6 @@ final class Version20240703091830 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE workout_user (workout_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_F51DD535A6CCCFC9 (workout_id), INDEX IDX_F51DD535A76ED395 (user_id), PRIMARY KEY(workout_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE workout_user ADD CONSTRAINT FK_F51DD535A6CCCFC9 FOREIGN KEY (workout_id) REFERENCES workout (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE workout_user ADD CONSTRAINT FK_F51DD535A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE workout DROP FOREIGN KEY FK_649FFB72A76ED395');
         $this->addSql('DROP INDEX IDX_649FFB72A76ED395 ON workout');
         $this->addSql('ALTER TABLE workout DROP user_id');
